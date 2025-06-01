c8700b34b96eedbf067bdbfe5be5d2ee9dee634c
Aayush Neupane <aayushneupane55@gmail.com>
2025-05-31 23:51:59 -0400

    New Add

--- src/app/orders/page.jsx
+++ src/app/orders/page.jsx
@@ -49,27 +49,27 @@ const Orders = () => {
     <AdminLayout>
       <div className="container p-2 mx-auto sm:p-4 text-white">
         <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
-            <div class="max-w pt-6">
-            <div class="relative flex items-center border border-black w-full h-full rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
-              <div class="grid place-items-center h-full w-12 text-gray-700">
+            <div className="max-w pt-6">
+            <div className="relative flex items-center border border-black w-full h-full rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
+              <div className="grid place-items-center h-full w-12 text-gray-700">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="h-6 w-6"
+                  className="h-6 w-6"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
                   />
                 </svg>
               </div>
 
               <input
-                class="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
+                className="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
                 type="text"
                 id="search"
                 placeholder="Search something.."
@@ -123,7 +123,7 @@ const Orders = () => {
         <div className="overflow-x-auto rounded-lg">
           <table className="w-full p-6 text-xs text-left whitespace-nowrap">
             <thead>
-              <tr className="dark:bg-gray-800">
+              <tr className="dark:bg-red-600">
                 <th className="p-3">Order ID</th>
                 <th className="p-3">Customer</th>
                 <th className="p-3">Pizza Type</th>
