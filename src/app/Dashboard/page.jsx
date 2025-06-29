1b8262d673c40d2aca20efc7b1d77a87a27ec7c1
Shishir Paudel <sishirpaudel7@gmail.com>
2025-06-28 21:54:16 -0400

    done

--- src/app/Dashboard/page.jsx
+++ src/app/Dashboard/page.jsx
@@ -1,10 +1,18 @@
+"use client";
+import React from "react";
 import AdminLayout from "../components/adminLayouts";
+import Lottie from "lottie-react";
+import MaintainanceAnimation from "../../../Assets/Maintainance.json";
 
 export default function Dashboard() {
   return (
-    <AdminLayout >
-    Hello
-    
+    <AdminLayout>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <h1 className="text-3xl font-bold mb-4">Under Maintenance</h1>
+        <div className="w-80 h-80">
+          <Lottie animationData={MaintainanceAnimation} loop={true} />
+        </div>
+      </div>
     </AdminLayout>
   );
 }
