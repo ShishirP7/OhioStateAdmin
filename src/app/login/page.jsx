cb4c90629139c397feba31c44a0ad8a9a177c0c0
Aayush Neupane <aayushneupane55@gmail.com>
2025-06-29 14:58:32 -0400

    sdad

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -21,11 +21,7 @@ const LoginForm = () => {
     setIsLoading(true);
     setError(null);    
     try {
-<<<<<<< HEAD
       const response = await fetch('api.ohiostatepizzas.com/api/employees/login', {
-=======
-      const response = await fetch('https://api.ohiostatepizzas.com/api/employees/login', {
->>>>>>> 6d3012232e0d127f706414a91ccfae66ea0d74e3
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
