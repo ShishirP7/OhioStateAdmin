7e1604598df7f98286e127b7469f0496ee9a0d95
Aayush Neupane <aayushneupane55@gmail.com>
2025-07-01 23:43:32 -0400

    Normal

--- src/app/Dashboard/page.jsx
+++ src/app/Dashboard/page.jsx
@@ -1,15 +1,16 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import AdminLayout from "../components/adminLayouts";
 import Lottie from "lottie-react";
-import MaintainanceAnimation from "../../assets/Maintainance.json";
+import MaintainanceAnimation from "../../../Assets/Maintainance.json";
 
 export default function Dashboard() {
+
   return (
     <AdminLayout>
-      <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center ">
         <div>
-          <Lottie animationData={MaintainanceAnimation} loop={true} />
+            <Lottie animationData={MaintainanceAnimation} loop={true} />
         </div>
       </div>
     </AdminLayout>
